06b1f00ca514c3003c4e00fd3f4922c0f933659a
Dmitriy <dstikhonov@rt-dc.ru>
2021-01-29 09:37:07 +0300

    Задание №16: Реализованы опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- fixture/application.py
+++ fixture/application.py
@@ -6,12 +6,20 @@ from fixture.group import groupHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox(executable_path=r'C:\\Users\proun\Desktop\Python\geckodriver.exe')
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(executable_path=r'C:\\Users\proun\Desktop\Python\geckodriver.exe')
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome(executable_path=r'C:\\Users\proun\Desktop\Python\chromedriver.exe')
+        elif browser == "ie":
+            self.wd = webdriver.Ie(executable_path=r'C:\\Users\proun\Desktop\Python\IEDriverServer.exe')
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(3)
         self.session = sessionHelper(self)
         self.contact = contactHelper(self)
         self.group = groupHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -22,7 +30,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
